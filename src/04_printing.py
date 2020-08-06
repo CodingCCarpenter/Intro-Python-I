ac1952b3aa0f5522740dbd38609b544ec50aa325
CodingCCarpenter <Christineassists@aol.com>
2020-08-05 22:29:48 -0500

    demonstrate f strings

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,7 +11,8 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
-
+formatted_y = "{:2f}".format(y)
+print(f'x is {x}, y is {formatted_y}, z is "{z}"')
 # Use the 'format' string method to print the same thing
 
 # Finally, print the same thing using an f-string
